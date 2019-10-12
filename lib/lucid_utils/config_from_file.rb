447f92c1080d22e10c7b252f8506465dcf20db77
Kelsey Judson <kelseyjudson@protonmail.com>
2019-10-12 03:13:28 -0500

    Remove unnecessary comment lines of padding

--- lib/lucid_utils/config_from_file.rb
+++ lib/lucid_utils/config_from_file.rb
@@ -2,12 +2,10 @@
 
 module LucidUtils
   class ConfigFromFile
-    #
     # @param path [String]
     # @param env_prefix [String, nil] environment variables override values in the file
     #
     # @return [Hash]
-    #
     def call(path, env_prefix: nil)
       instance_eval(File.read(path)).tap do |result|
         raise TypeError, 'not and instance of Hash' unless result.is_a?(Hash)
@@ -17,12 +15,10 @@ module LucidUtils
     end
 
     
-    #
     # @param config [Hash]
     # @param env_prefix [String]
     #
     # @return [Hash]
-    #
     private def env_overrides(config, env_prefix)
       config.each_with_object({}) do |(k, v), config|
         env_key = [env_prefix, k].join('_').upcase
